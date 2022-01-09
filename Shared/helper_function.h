7607992a5650418f80efc84272ea80c747cc011a
ofira1 <attiaofir1@gmail.com>
2022-01-10 01:39:16 +0200

    to ariel

--- Shared/helper_function.h
+++ Shared/helper_function.h
@@ -5,32 +5,31 @@
 
 
 /// <summary>
-/// 
+/// create file and write into it
 /// </summary>
-/// <param name="write_file_name"></param>
-/// <param name="message"></param>
+/// <param name="write_file_name">log file name</param>
+/// <param name="message">message from/to server</param>
 /// <returns></returns>
 int write_to_file(char* write_file_name, char* message);
 /// <summary>
-/// 
+/// write input to log file
 /// </summary>
-/// <param name="start"></param>
-/// <param name="log_file"></param>
-/// <param name="input"></param>
+/// <param name="log_file">log file name</param>
+/// <param name="input">input to write to log</param>
 /// <returns></returns>
 int write_to_log_file(char* log_file, char* input);
 /// <summary>
-/// 
+/// free nessage struct
 /// </summary>
-/// <param name="message"></param>
+/// <param name="message">message from/to server</param>
 void free_message(Message* message);
 /// <summary>
-/// 
+/// recieve input and decode it and store into message struct
 /// </summary>
-/// <param name="input"></param>
-/// <param name="message"></param>
-/// <param name="send_or_recv"></param>
-/// <returns></returns>
+/// <param name="input">input to decode</param>
+/// <param name="message">message from/to server</param>
+/// <param name="send_or_recv">mode of decode for print</param>
+/// <returns>return SUCCESS_CODE if no errors</returns>
 int decode_message(char* input,Message * message,char * send_or_recv);
 #endif
 
