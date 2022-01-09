bae78532d09ee2e541d181918fc3d2c4fdc4d687
ofira1 <attiaofir1@gmail.com>
2022-01-09 22:24:13 +0200

    client fsm doc

--- client/state_machine_client.h
+++ client/state_machine_client.h
@@ -5,49 +5,49 @@
 #include "../shared/helper_function.h"
 
 /// <summary>
-/// 
+/// connecting to server
 /// </summary>
-/// <param name="argv"></param>
-/// <returns></returns>
+/// <param name="argv"><arguments/param>
+/// <returns> returns next state </returns>
 int state0(char* argv[]);
 /// <summary>
-/// 
+/// let the client chhose if to play or quit
 /// </summary>
-/// <param name="argv"></param>
-/// <returns></returns>
+/// <param name="argv">arguments</param>
+/// <returns> returns next state </returns>
 int state1(char* argv[]);
 /// <summary>
-/// 
+///  starting game
 /// </summary>
-/// <param name="argv"></param>
-/// <returns></returns>
+/// <param name="argv">arguments</param>
+/// <returns> returns next state </returns>
 int state2(char* argv[]);
 /// <summary>
-/// 
+/// switch turns between opponents and lets client know whos the winner
 /// </summary>
-/// <param name="argv"></param>
-/// <returns></returns>
+/// <param name="argv">arguments</param>
+/// <returns> returns next state </returns>
 int state3(char* argv[]);
 /// <summary>
-/// 
+///  disconnection
 /// </summary>
 /// <returns></returns>
 int state4();
 /// <summary>
-/// 
+/// when connection failes lets the client chhose if to reconnect
 /// </summary>
-/// <param name="clientService"></param>
-/// <param name="argv"></param>
-/// <returns></returns>
+/// <param name="clientService">client socket</param>
+/// <param name="argv">arguments</param>
+/// <returns> returns 0 if no errors </returns>
 int state5(SOCKADDR_IN clientService, char* argv[]);
 /// <summary>
-/// 
+/// manage client set of actions and messages
 /// </summary>
-/// <param name="clientService"></param>
-/// <param name="ip"></param>
-/// <param name="port"></param>
-/// <param name="argv"></param>
-/// <param name="file_name"></param>
+/// <param name="clientService">client socket</param>
+/// <param name="ip">client ip</param>
+/// <param name="port">client port</param>
+/// <param name="argv">arguments</param>
+/// <param name="file_name">name of log file</param>
 /// <returns></returns>
 int state_machine(SOCKADDR_IN clientService, char* ip, int port, char* argv[], char file_name[MAX_LINE]);
 
